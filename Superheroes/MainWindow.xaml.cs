43ca286c3bf667a537714aa69ec04647444c403f
Antonio Murcia <amurciaa61@gmail.com>
2020-11-27 19:35:54 +0100

    Agregar archivos de proyecto.

---
+++ Superheroes/MainWindow.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+
+
+namespace Superheroes
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
