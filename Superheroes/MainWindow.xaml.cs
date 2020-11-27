dc7ba83725e8d1e0ee48b3ef9a642eb2792e4314
Antonio Murcia <amurciaa61@gmail.com>
2020-11-27 21:59:12 +0100

    Incluyo logica para mostrar las imagenes y el nombre conforme pulsan flecha adelande o atras, actualizo el TextBlock n/n

--- Superheroes/MainWindow.xaml.cs
+++ Superheroes/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows;
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
 
 
 namespace Superheroes
@@ -8,9 +10,54 @@ namespace Superheroes
     /// </summary>
     public partial class MainWindow : Window
     {
+        List<Superheroe> heroes;
+        
         public MainWindow()
         {
             InitializeComponent();
+            heroes = Superheroe.GetSamples();
+            BindingDelObjeto(1);
+            ActualizaIndice(1);
+        }
+
+        private void flechaAdelanteImage_MouseUp(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            string valorActual = numeroImagenTextBlock.Text;
+            string[] valores = valorActual.Split('/');
+            int indiceActual = Int32.Parse(valores[0]);
+            int indiceMaximo = heroes.Count;
+            int indice;
+            if (indiceActual == indiceMaximo)
+                indice = 1;
+            else
+                indice = indiceActual + 1;
+            BindingDelObjeto(indice);
+            ActualizaIndice(indice);
+        }
+        private void flechaAtrasImage_MouseUp(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            string valorActual = numeroImagenTextBlock.Text;
+            string[] valores = valorActual.Split('/');
+            int indiceActual = Int32.Parse(valores[0]);
+            int indiceMaximo = heroes.Count;
+            int indice;
+            if (indiceActual == 1)
+                indice = indiceMaximo;
+            else
+                indice = indiceActual - 1;
+            BindingDelObjeto(indice);
+            ActualizaIndice(indice);
+        }
+        public void BindingDelObjeto(int indice)
+        {
+            indice--;
+            nombreHeroeTextBlock.DataContext = heroes[indice];
+            imagenHeroeVillanoImage.DataContext = heroes[indice];
+        }
+        public void ActualizaIndice(int indice)
+        {
+            numeroImagenTextBlock.Text = indice.ToString()+"/"+heroes.Count.ToString();
+           // MessageBox.Show(indice.ToString() + "/" + heroes.Count.ToString());
         }
     }
 }
